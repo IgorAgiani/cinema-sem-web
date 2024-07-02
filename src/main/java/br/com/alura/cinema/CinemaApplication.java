cb4da3409780d2d9f18480a14d5e7154cc6555c8
IgorAgiani <igor_011@hotmail.com.br>
2024-07-02 09:29:37 -0300

    inicio-do-projeto

---
+++ src/main/java/br/com/alura/cinema/CinemaApplication.java
@@ -0,0 +1,29 @@
+package br.com.alura.cinema;
+
+import br.com.alura.cinema.model.DadosSerie;
+import br.com.alura.cinema.service.ConsumoApi;
+import br.com.alura.cinema.service.ConverterDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=b76cfa9");
+		System.out.println(json);
+
+		ConverterDados conversor = new ConverterDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
+
